ab202255b9d8b4f2eee324ed7c121c86c870a665
higer <higer@gmail.com>
2019-09-08 23:49:34 +1000

    use onLoad event to calculate gridRowEnd

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function ImageCard(props) {
-  const { id, description, urls } = props;
-  return <img key={id} alt={description} src={urls.regular} />;
+export default class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.imgRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+
+  loadFinished = e => {
+    console.log(this.imgRef.current.clientHeight);
+    const spans = Math.ceil(this.imgRef.current.clientHeight / 10);
+    this.setState({
+      ...this.state,
+      spans
+    });
+  };
+
+  componentDidMount() {}
+
+  render() {
+    const { id, description, urls } = this.props;
+    return (
+      <img
+        style={{ gridRowEnd: `span ${this.state.spans}` }}
+        ref={this.imgRef}
+        onLoad={this.loadFinished}
+        key={id}
+        alt={description}
+        src={urls.regular}
+      />
+    );
+  }
 }
